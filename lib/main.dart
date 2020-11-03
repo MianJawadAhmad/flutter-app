63f1f0cb6b4a1569e70ecba1d7ce3bbcf2cffebc
MianJawadAhmad <a.jawad321@gmail.com>
2020-11-03 20:09:04 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return MaterialApp(
+      title: 'Welcome to Flutter1',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(''),
+        ),
+        body: Center(
+          child: Text(wordPair.asPascalCase),
+        ),
+      ),
+    );
+  }
+}
